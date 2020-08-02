992ca9f302cffa2b452f55914e3526c1a518a93b
Jonathan <jcl00087@gmail.com>
2020-08-02 00:48:35 -0400

    Print statements

--- python/EpicToolbox/MachineLearning/FeatureSelection/sequentialfs.py
+++ python/EpicToolbox/MachineLearning/FeatureSelection/sequentialfs.py
@@ -14,12 +14,12 @@ model(X) to predict
 def cv_score(model,scorer,X,Y,features=None,cv=None):
     ''' Perform a cross validation scoring
     cvscore(model,scorer,X,Y,cv=None)
-    return a tuple containing the average score 
-    of the cross validation followed by individual cv scores.    
-    '''            
+    return a tuple containing the average score
+    of the cross validation followed by individual cv scores.
+    '''
     if features is None:
         features=np.arange(0,X.shape[1])
-    X=X[:,features]    
+    X=X[:,features]
     if cv is None:
         modeli = clone(model)
         modeli.fit(X,Y)
@@ -29,27 +29,27 @@ def cv_score(model,scorer,X,Y,features=None,cv=None):
     else:
         scorecvi=[]
         for train, test in cv.split(X):
-            modeli = clone(model)            
+            modeli = clone(model)
             modeli.fit(X[train, :], Y[train])
             scorecvi.append(scorer(modeli,X[test, :], Y[test]))
         scores=np.mean(scorecvi)
         other={'CVScores':scorecvi}
-    
+
     return scores,other
 
-def multisubject_cv_score(model,scorer,subjectsX,subjectsY,features=None,cv=None):    
-    ''' Perform a cross validation scoring for multiple subjects for subject 
+def multisubject_cv_score(model,scorer,subjectsX,subjectsY,features=None,cv=None):
+    ''' Perform a cross validation scoring for multiple subjects for subject
     dependent models. Calls cvs_core(model,scorer,X,Y,cv=None) on each subject
     and returns the global score (average score across subject) and the within
     subjects within cv scores.
     '''
     if features is None:
         features=np.arange(0,X.shape[1])
-        
+
     subjectsScores=[]
     subjectsOthers=[]
-    
-    for i in range(0,len(subjectsX)):        
+
+    for i in range(0,len(subjectsX)):
         X=subjectsX[i]
         X=X[:,features]
         Y=subjectsY[i]
@@ -62,58 +62,61 @@ def multisubject_cv_score(model,scorer,subjectsX,subjectsY,features=None,cv=None
 
 class SequentialFeatureSelection:
     ''' Sequential Feature Selection
-    
+
     A class to do sequential feature selection with open input to evaluation functions and
-    progress checkpoint saving. 
-    
+    progress checkpoint saving.
+
     #ONLY SUPPORTS FORWARD Right now#
-    
-    #How to use:        
+
+    #How to use:
     # Create a SequentialFeatureSelection object
     SequentialFeatureSelection(criterion_function,total_features,keepin=None,n_jobs=1)
-                   
-    criterion_fun: function to run and compute the score metric. 
+
+    criterion_fun: function to run and compute the score metric.
     The function must be of form criterion_fun(features). This function must return
     a tuple pair where the first value is a scalar score and the second value a dictionary
     with other outputs to be saved.
-    
+
     keepin: is a list of feature indices to always keep in the selection
-    
+
     n_jobs: parallelize to n_jobs
-        
-        
+
+
     '''
     def __init__(self,fun,total_features,keepin=None,n_jobs=1,verbose=0):
-                
+
         self.total_features=total_features #Total number of features
-        
+
         self.allfeatures=set(range(0,self.total_features))
         self.n_jobs=n_jobs
         self._fun=fun
         self.verbose=verbose
         #self._isready=False #To tell if the object is ready to run the sequential selection
-        
+
         self._iterations=0 #Number of iterations (both forward or backward increment this)
         self._results={} #List of results (one per iteration)
 
         # Included is a list of feature indices that went into the selected pool
         if keepin is not None:
-            self.included=keepin            
+            self.included=keepin
         else:
-            self.included=[]            
-                        
+            self.included=[]
+
 
     def _inclusion(self,included):
         ''' Select one new feature by testing from all the features that have not been
         included by testing the fun with each one of them added
-        
-        _inclusion(self,included) 
-        
+
+        _inclusion(self,included)
+
         included is a set of features. Returns bestfeature, bestscore and bestresults
         '''
-        
+
+        if self.verbose>0:
+            print("Running inclusion to find features {} + ?".format(included))
+
         remaining=self.allfeatures - included
-        
+
         # Use joblib to run the scores in parallel to find the scores for
         # each remaining feature
         if remaining:
@@ -121,11 +124,10 @@ class SequentialFeatureSelection:
             n_jobs = min(self.n_jobs, features)
             parallel = Parallel(n_jobs=n_jobs, verbose=self.verbose)
             work = parallel(delayed(self._fun)
-                            (tuple(set(self.included) | {feature}))
+                            (tuple(set(included) | {feature}))
                             for feature in remaining)
-
             allres = np.array(work)
-            
+
             featscores=[res[0] for res in work]
             other=[res[1] for res in work]
 
@@ -134,20 +136,23 @@ class SequentialFeatureSelection:
             bestfeature = list(remaining)[idx]
             bestscore = featscores[idx]
             bestresults = other[idx]
+
+            if self.verbose>0:
+                print("Best feature {} added with score {}".format(bestfeature,bestscore))
+
         return bestfeature, bestscore, bestresults
 
     def _exclusion(self,included):
         ''' Deselect one new feature by testing from all the features that have been
-        included by testing the fun with each one of them removed and see which one
         makes the best performance.
-        
-        _exclusion(self,included) 
-        
+
+        _exclusion(self,included)
+
         included is a set of features. Returns bestfeature, bestscore and bestresults
         '''
-        
+
         remaining= included
-        
+
         # Use joblib to run the scores in parallel to find the scores for
         # each remaining feature
         if remaining:
@@ -159,7 +164,7 @@ class SequentialFeatureSelection:
                             for feature in remaining)
 
             allres = np.array(work)
-                                                
+
             featscores=[res[0] for res in work]
             other=[res[1] for res in work]
 
@@ -174,28 +179,27 @@ class SequentialFeatureSelection:
 
     def forward(self,nsteps):
         ''' Run a forward selection process for nsteps'''
-        
+
         nIncluded=len(self.included)
         # Add nsteps more features
         if len(self.included)<self.total_features:
-            if nsteps>self.total_features-nIncluded:            
+            if nsteps>self.total_features-nIncluded:
                 nsteps=self.total_features-nIncluded
-            
-            print("running forward for {} nsteps".format(nsteps))                
-            for i in range(0,nsteps):                            
+
+            print("running forward for {} nsteps".format(nsteps))
+            for i in range(0,nsteps):
                 (bestfeature,bestscore,bestresults)=self._inclusion(set(self.included))
                 self.included=list(set(self.included) | set([bestfeature]))
-                inclusion_results={'FeaturesIn': self.included, 
+                inclusion_results={'FeaturesIn': self.included,
                               'Score': bestscore,
                               'Other': bestresults}
                 self._results[self._iterations]=inclusion_results
                 self._iterations=self._iterations+1
-            
-            
+
         else:
             print("No more features left skipping forward")
-        
-        
+
+
     def backward(self,nsteps):
         ''' Run a backward selection (elimination) process for nsteps'''
         nIncluded=len(self.included)
@@ -203,71 +207,72 @@ class SequentialFeatureSelection:
         if len(self.included)>0:
             if nIncluded-nsteps<1:
                 nsteps=nIncluded+1
-            
-            print("running forward for {} nsteps".format(nsteps))                
-            for i in range(0,nsteps):                            
+                print("bestfeature {}".format(bestfeature))
+                print("bestscore {}".format(bestscore))
+
+            print("running forward for {} nsteps".format(nsteps))
+            for i in range(0,nsteps):
                 (bestfeature,bestscore,bestresults)=self._exclusion(set(self.included))
+                #included by testing the fun with each one of them removed and see which one
                 self.included=list(set(self.included) - set([bestfeature]))
-                exclusion_results={'FeaturesIn': self.included, 
+                exclusion_results={'FeaturesIn': self.included,
                               'Score': bestscore,
                               'Other': bestresults}
                 self._results[self._iterations]=exclusion_results
                 self._iterations=self._iterations+1
-            
-            
+
+
         else:
             print("No more features left skipping forward")
-        
-        
-        
-        
+
+
+
+
     def load(self,filename):
         ''' Load data from HDF5 file'''
         if not os.path.isfile(filename):
-            print('File does not exist'+ filename)        
+            print('File does not exist'+ filename)
         else:
             try:
-                data=io.loadmat(filename,squeeze_me=True)                
+                data=io.loadmat(filename,squeeze_me=True)
                 print('Previous results file found: ' + filename)
                 self._iterations=len(data['FeaturesIn'])
-                self.included=data['FeaturesIn'][self._iterations-1]                
+                self.included=data['FeaturesIn'][self._iterations-1]
                 for i in range(0,self._iterations):
                     results_dict={'FeaturesIn':data['FeaturesIn'][i],
                                  'Score':data['Score'][i],
                                  'Other':data['Other'][i]}
                     self._results[i]=results_dict
-                
-                
-            except Exception as e:                
+
+
+            except Exception as e:
                 print("mierda")
                 print(e)
-                    
-    
+
+
     def save(self,filename):
         ''' Save data to HDF5 file
-        The results are saved to a mat file containing the variables:        
+        The results are saved to a mat file containing the variables:
         FeaturesIn: a cell array size N,1 N=number of iterations of the process.
         Score: a cell array with the corresponding scores of each iteration.
-        Other: a cell array with the other results collected per iteration.                
+        Other: a cell array with the other results collected per iteration.
         '''
-        
+
         #self.results[i] is the ith iteration of results to be saved it is a
         #dictionary containing FeaturesIn, Score and Other
-        
+
         featuresIn=[self._results[i]['FeaturesIn'] for i in range(0,self._iterations)]
         score=[self._results[i]['Score'] for i in range(0,self._iterations)]
         other=[self._results[i]['Other'] for i in range(0,self._iterations)]
-        
+
         results={'FeaturesIn':featuresIn,
                  'Score':score,
                  'Other':other}
         io.savemat(filename,results)
-        
-    def __str__(self):        
+
+    def __str__(self):
         strmsg='Sequential Feature Selection\n'
         strmsg=strmsg+'Iterations completed: {}\n'.format(self._iterations)
         strmsg=strmsg+'Total possible features: {}\n'.format(self.total_features)
         strmsg=strmsg+'Features In: {}\n'.format(self.included)
         return strmsg
-                                                 
-       
